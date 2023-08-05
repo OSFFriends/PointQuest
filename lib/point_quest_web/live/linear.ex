1e34942572286ac448308a6cb18dd2aaf3066077
Jason Noonan <jason.noonan@pdq.com>
2023-08-05 17:04:00 -0500

    feat: add liveview for linear

---
+++ lib/point_quest_web/live/linear.ex
@@ -0,0 +1,27 @@
+defmodule PointQuestWeb.Linear do
+  @moduledoc """
+  Test landing page for logging in to linear
+  """
+  use PointQuestWeb, :live_view
+
+  @linear_config Application.compile_env(:point_quest, Infra.Linear)
+
+  def render(assigns) do
+    ~H"""
+    <button>Linear</button>
+    """
+  end
+
+  def mount(_params, _session, socket) do
+    client =
+      OAuth2.Client.new(
+        strategy: Strategy.AuthCode,
+        client_id: @linear_config[:client_id],
+        client_secret: @linear_config[:client_secret],
+        site: "https://linear.app"
+      )
+
+    socket = assign(socket, client: client)
+    {:ok, socket}
+  end
+end
