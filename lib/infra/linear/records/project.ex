c566bfd9c17438b4c8ae5dca9c2563d0ca260979
Jason Noonan <jason.noonan@pdq.com>
2024-02-24 15:48:44 -0600

    feat: include project linear schema

---
+++ lib/infra/linear/records/project.ex
@@ -0,0 +1,17 @@
+defmodule Infra.Linear.Records.Project do
+  @moduledoc """
+  A linear project, used to group "buckets" of issues.
+  """
+
+  use Infra.LinearObject
+
+  @type project :: %__MODULE__{
+          id: String.t(),
+          name: String.t()
+        }
+
+  object do
+    field :id, :string
+    field :name, :string
+  end
+end
