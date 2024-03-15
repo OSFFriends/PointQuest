97c41df30521b5d11a35b943200a4dce5827aa4a
Jason Noonan <jason.noonan@pdq.com>
2024-03-15 15:52:41 -0500

    refactor: rip out user and linear auth danglers
    We are aiming to simplify the current app state in order to get up and running.
    
    To this end, we are removing the current implementation of user auth and linear auth.
    
    Aiming to keep the starting app as low-state as possible.

--- lib/point_quest_web/live/quest.ex
+++ lib/point_quest_web/live/quest.ex
@@ -3,79 +3,27 @@ defmodule PointQuestWeb.Quest do
   Retrieve and display quest information
   """
   use PointQuestWeb, :live_view
-  use Tesla
-
-  @spec linear() :: module()
-  def linear(), do: Application.get_env(:point_quest, PointQuest.Behaviour.Linear)
 
   def render(assigns) do
     ~H"""
     <div class="flex flex-row items-start gap-y-5">
-      <div :if={!is_nil(@team)} class="flex flex-col items-start self-stretch order-none gap-y-5">
-        <div>
-          <h2 class="text-2xl">Teams</h2>
-          <p :for={%{"name" => name, "id" => id} <- @teams}><%= name %> - <%= id %></p>
-        </div>
-        <div id="lists" class="grid sm:grid-cols-1 md:grid-cols-3 gap-2">
-          <.live_component
-            id="1"
-            module={PointQuestWeb.Live.Components.List}
-            list={@issues}
-            list_name="Issues"
-            session={@session}
-          />
-        </div>
-      </div>
-      <div>
-        <p :if={not is_nil(@current_ticket)}><%= @current_ticket %></p>
-      </div>
+      <button id="start quest" phx-click="start_quest">Start Quest</button>
     </div>
     """
   end
 
   def mount(_params, _session, socket) do
     session_id = "12242ljl"
-    current_user = socket.assigns.current_user.id
-    teams = linear().list_teams(current_user)
-    team = List.first(teams)
-    issues = linear().list_issues(team["id"], current_user)
 
     socket =
       assign(socket,
-        teams: teams,
-        team: team,
-        issues: issues,
-        session: session_id,
-        current_ticket: nil
+        session: session_id
       )
 
     {:ok, socket}
   end
 
-  def handle_event("update-sorting", params, socket) do
-    message = {:list_updated, params}
-    Phoenix.PubSub.broadcast(PointQuest.PubSub, get_session(socket), message)
+  def handle_event("start_quest", _params, socket) do
     {:noreply, socket}
-  end
-
-  def handle_event("select-ticket", %{"ticketnumber" => ticket}, socket) do
-    issue = linear().load_issue(ticket, socket.assigns.current_user.id)
-
-    {:noreply, assign(socket, :current_ticket, issue.identifier)}
-  end
-
-  defp get_session(socket) do
-    "session:" <> socket.assigns.session
-  end
-
-  def handle_info({:list_updated, %{"ids" => id_list}}, socket) do
-    issues = socket.assigns.issues
-
-    issues =
-      for id <- id_list do
-        Enum.find(issues, fn %{id: i_id} -> String.to_integer(id) == i_id end)
-      end
-
-    {:noreply, assign(socket, :issues, issues)}
   end
 end
