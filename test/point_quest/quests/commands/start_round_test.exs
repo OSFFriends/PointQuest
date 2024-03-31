fc2ee6b2979775e1424a24e9e9ba30a901cbf161
Jason Noonan <jason.noonan@pdq.com>
2024-03-31 13:58:58 -0500

    feat: introduce round_started event and start_round command
    In order to constrain the behavior of when adventurers are allowed to attack, we need
    to introduce the concept of a "round". A round is a collection of attacks against a
    single issue.
    
    Rounds are tracked on the quest state in the `round_active?` boolean field.

---
+++ test/point_quest/quests/commands/start_round_test.exs
@@ -0,0 +1,94 @@
+defmodule PointQuest.Quests.Commands.StartRoundTest do
+  use ExUnit.Case
+
+  alias PointQuest.Quests.Commands.StartRound
+  alias PointQuest.Quests.Event.RoundStarted
+
+  setup do
+    {:ok, QuestSetupHelper.setup()}
+  end
+
+  describe "new/1" do
+    test "returns ok tuple if valid", %{quest: %{id: quest_id}} do
+      assert {:ok, %StartRound{quest_id: ^quest_id}} =
+               StartRound.new(%{quest_id: quest_id})
+    end
+
+    test "returns error tuple if quest id omitted" do
+      assert {:error, %{valid?: false}} = StartRound.new(%{})
+    end
+  end
+
+  describe "new!/1" do
+    test "returns command if valid", %{quest: %{id: quest_id}} do
+      assert %StartRound{quest_id: ^quest_id} = StartRound.new!(%{quest_id: quest_id})
+    end
+
+    test "throws if quest id is omitted" do
+      assert_raise Ecto.InvalidChangesetError, fn -> StartRound.new!(%{}) end
+    end
+  end
+
+  describe "changeset/2" do
+    test "returns changeset if valid" do
+      assert %{valid?: true, changes: %{quest_id: "abc123"}} =
+               StartRound.changeset(%StartRound{}, %{quest_id: "abc123"})
+    end
+
+    test "returns error changeset if invalid" do
+      assert %{valid?: false, errors: [quest_id: {"can't be blank", _validation}]} =
+               StartRound.changeset(%StartRound{}, %{})
+    end
+  end
+
+  describe "execute/2" do
+    test "returns event when successful", %{quest: %{id: quest_id}, party_leader_actor: actor} do
+      assert {:ok, %RoundStarted{quest_id: ^quest_id}} =
+               %{quest_id: quest_id}
+               |> StartRound.new!()
+               |> StartRound.execute(actor)
+    end
+
+    test "returns error if quest doesn't exist", %{party_leader_actor: actor} do
+      assert {:error, %PointQuest.Error.NotFound{resource: :quest}} =
+               %{quest_id: "abc123"}
+               |> StartRound.new!()
+               |> StartRound.execute(actor)
+    end
+
+    test "returns error if actor is not party leader of provided quest", %{
+      quest: %{id: quest_id},
+      other_actor: actor
+    } do
+      assert {:error, :must_be_leader_of_quest_party} =
+               %{quest_id: quest_id}
+               |> StartRound.new!()
+               |> StartRound.execute(actor)
+    end
+
+    test "returns error if round is started by non-party leader", %{
+      quest: %{id: quest_id},
+      adventurer_actor: actor
+    } do
+      assert {:error, :must_be_leader_of_quest_party} =
+               %{quest_id: quest_id}
+               |> StartRound.new!()
+               |> StartRound.execute(actor)
+    end
+
+    test "returns error if round is already active on provided quest", %{
+      quest: %{id: quest_id},
+      party_leader_actor: actor
+    } do
+      assert {:ok, %RoundStarted{}} =
+               %{quest_id: quest_id}
+               |> StartRound.new!()
+               |> StartRound.execute(actor)
+
+      assert {:error, :round_already_active} =
+               %{quest_id: quest_id}
+               |> StartRound.new!()
+               |> StartRound.execute(actor)
+    end
+  end
+end
